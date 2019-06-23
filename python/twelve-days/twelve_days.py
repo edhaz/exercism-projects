47a4197ea20c69bd924f4f7becb7d18e090c2e61
Ed Hazledine <ed.hazledine@gmail.com>
2019-06-23 10:53:55 +0100

    Finish twelve-days

--- python/twelve-days/twelve_days.py
+++ python/twelve-days/twelve_days.py
@@ -1,36 +1,32 @@
+from functools import lru_cache
+
+lyrics = [
+    ("first", "and a Partridge in a Pear Tree."),
+    ("second", "two Turtle Doves,"),
+    ("third", "three French Hens,"),
+    ("fourth", "four Calling Birds,"),
+    ("fifth", "five Gold Rings,"),
+    ("sixth", "six Geese-a-Laying,"),
+    ("seventh", "seven Swans-a-Swimming,"),
+    ("eighth", "eight Maids-a-Milking,"),
+    ("ninth", "nine Ladies Dancing,"),
+    ("tenth", "ten Lords-a-Leaping,"),
+    ("eleventh", "eleven Pipers Piping,"),
+    ("twelfth", "twelve Drummers Drumming,")
+]
+
+
 def recite(start_verse, end_verse):
-
-    for day in range(start_verse - 1, end_verse):
-        line = []
-        if day + 1 == 1:
-            line.append("a Partridge in a Pear Tree")
-        else:
-            for n in range(day + 1, 0, -1):
-                line.append(days[n - 1][1])
-        song.append(opening.format(days[day][0], ", ".join(line)))
+    return [build_line(day) for day in range(start_verse, end_verse + 1)]
 
 
-def build_verse(days):
-    verse = "On the {} day of Christmas my true love gave to me: {}."
-    return verse
+@lru_cache(maxsize=12)
+def build_line(day):
+    nth = lyrics[day - 1][0]
+    first_line = f"On the {nth} day of Christmas my true love gave to me:"
+    gift_line = "a Partridge in a Pear Tree." if day == 1 else build_verse(day)
+    return f"{first_line} {gift_line}"
 
 
-def build_lines():
-    days = [
-        ("first", "and a Partridge in a Pear Tree"),
-        ("second", "two Turtle Doves"),
-        ("third", "three French Hens"),
-        ("fourth", "four Calling Birds"),
-        ("fifth", "five Gold Rings"),
-        ("sixth", "six Geese-a-Laying"),
-        ("seventh", "seven Swans-a-Swimming"),
-        ("eighth", "eight Maids-a-Milking"),
-        ("ninth", "nine Ladies Dancing"),
-        ("tenth", "ten Lords-a-Leaping"),
-        ("eleventh", "eleven Pipers Piping"),
-        ("twelfth", "twelve Drummers Drumming")
-    ]
-
-
-def build_song():
-    pass
+def build_verse(day):
+    return " ".join([lyrics[num][1] for num in reversed(range(day))])
